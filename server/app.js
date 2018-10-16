60b857f34636f2b29c647a836abd12274639d7a8
Konstantin Kovalev <constantiner@gmail.com>
2018-10-16 17:10:35 +0200

    Add serving index.html

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import createError from "http-errors";
 import logger from "morgan";
 import path from "path";
-import indexRouter from "./routes";
+import { realpath } from "fs";
 import usersRouter from "./routes/users";
 import envConfig from "./util/envConfig";
+import { promisify } from "util";
+
+const realpathAsync = promisify(realpath);
 
 const getApp = async () => {
 	await envConfig("server/.env");
+	const cwd = await realpathAsync(process.cwd());
 
 	const app = express();
 
@@ -15,10 +20,27 @@ const getApp = async () => {
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: false }));
 	app.use(cookieParser());
-	app.use(express.static(path.join(__dirname, "public")));
+	app.use(express.static(path.join(cwd, "public")));
 
-	app.use("/", indexRouter);
 	app.use("/users", usersRouter);
+	app.use("/*", (req, res) => {
+		res.sendFile(path.join(cwd, "/public/index.html"));
+	});
+
+	// catch 404 and forward to error handler
+	app.use((req, res, next) => {
+		next(createError(404));
+	});
+
+	// error handler
+	/* eslint-disable-next-line no-unused-vars */
+	app.use((err, req, res, next) => {
+		/* eslint-disable-next-line no-console */
+		console.error(err);
+		res.status(err.status || 500).json({
+			error: err.message
+		});
+	});
 
 	return app;
 };
