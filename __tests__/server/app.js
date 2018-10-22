74e200c1392356151e20dc3ca86089304c0be6af
Konstantin Kovalev <constantiner@gmail.com>
2018-10-22 17:37:19 +0200

    Add express server tests with supertest

--- __tests__/server/app.js
+++ __tests__/server/app.js
@@ -1,7 +1,14 @@
 import getApp from "../../server/app";
+import request from "supertest";
 
 describe("Main server application tests", () => {
 	it("should export app function", () => {
 		expect(getApp).toBeDefined();
+	});
+	it("should respond for users get route", async () => {
+		expect.assertions(1);
+		const app = await getApp();
+		const response = await request(app).get("/users");
+		expect(response.statusCode).toBe(200);
 	});
 });
