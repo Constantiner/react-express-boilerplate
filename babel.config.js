3a46e8dc0ff033de0cab3e71f28a1e485ec96327
Konstantin Kovalev <constantiner@gmail.com>
2018-10-22 14:17:11 +0200

    Server tests are now work fine with Jest and Babel 7 support

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
-module.exports = function(api) {
-	api.cache(true);
-	const plugins = [];
-	const presets = [];
-	return {
-		plugins,
-		presets
+module.exports = api => {
+	const config = {
+		presets: [],
+		plugins: []
 	};
+	if (api.env("test")) {
+		config.presets.push("@babel/preset-env");
+	}
+	return config;
 };
