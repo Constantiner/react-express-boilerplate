cbdbfc370138e0ce5a5ffcb267cba4b584095784
Konstantin Kovalev <constantiner@gmail.com>
2018-10-23 13:46:51 +0200

    Add log4javascript support for client side

--- client/App.js
+++ client/App.js
@@ -1,6 +1,19 @@
+import log4javascript from "log4javascript";
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import logo from "./logo.svg";
+
+const consoleAppender = new log4javascript.BrowserConsoleAppender();
+const layout = new log4javascript.PatternLayout(`%d{${log4javascript.PatternLayout.DATETIME_DATEFORMAT}} [%-5p] (%c) %m`);
+log4javascript.setShowStackTraces(true);
+consoleAppender.setLayout(layout);
+const log = log4javascript.getRootLogger();
+log.addAppender(consoleAppender);
+log.setLevel(log4javascript.Level.DEBUG);
+
+const appLogger = log4javascript.getLogger("app");
+
+appLogger.debug("start!");
 
 class App extends Component {
 	render() {
